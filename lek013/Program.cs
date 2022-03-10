7c80d9dbc077226be9c9197c53ad2342a6bdba2f
Antonevich <v.antonevich@mail.ru>
2022-03-10 17:40:42 +0300

    сумма рандомных чисел

---
+++ lek013/Program.cs
@@ -0,0 +1,6 @@
+﻿int namberA = new Random().Next(1,23); // случайное число max-1 в данном случае 22
+Console.WriteLine(namberA);
+int namberВ = new Random().Next(1,20);
+Console.WriteLine(namberВ);
+int result = (namberA+namberВ);
+Console.WriteLine(result);
