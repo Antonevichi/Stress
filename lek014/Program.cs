3a3dfedf37e2e9d728b5ad2a5274927beaa46202
Antonevich <v.antonevich@mail.ru>
2022-03-10 17:39:37 +0300

    Определяли любимчиков по имени

---
+++ lek014/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите ваше имя:   ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "слава") 
+{
+    Console.WriteLine ("Ура! Это наш Супергерой!!!");
+}
+
+else
+{
+    Console.Write ("Привет  ");
+    Console.Write (username);
+}
