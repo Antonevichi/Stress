9b905b36a4c02cecd3f0473e2e195f165502915e
Antonevich <v.antonevich@mail.ru>
2022-03-10 17:55:52 +0300

    нахождение максимального значения

---
+++ lek015/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 3;
+int b = 45;
+int c = 8;
+int d = 4;
+int e = 2;
+
+int max = a;
+if (a>max);
+  max=a;
+  if (b>max)
+  max=b;
+  if (c>max)
+  max=c;
+  if (d>max)
+  max=d;
+  if (e>max)
+  max=e;
+
+Console.Write("Максимальное из заданных чисел является - ");
+Console.Write(max);
